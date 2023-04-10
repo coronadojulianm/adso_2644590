2426589b6f415232123c50ebd5980efe1cb9ed2d
lucas <lasknfaidbafaso@gamil.com>
2023-04-10 11:39:30 -0500

    Añadir js a media query

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let menuHamburguer = document.getElementById("menuHamburguer")
+let mainMenu= document.getElementById("mainMenu")
+
+menuHamburguer.addEventListener("click", function (){
+    
+mainMenu.classList.toggle("menu-hidden")
+})  
