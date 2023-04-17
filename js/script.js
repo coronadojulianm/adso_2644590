1878e76faa8efb40b25756232d7e80d56780d918
coronadojulianm <juliancoronadom@gmail.com>
2023-04-17 11:50:27 -0500

    Comit nuevas pag

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-let menuHamburguer = document.getElementById("menuHamburguer")
-let mainMenu= document.getElementById("mainMenu")
+let menuHamburguer = document.getElementById("menuHamburguer");
+let mainMenu= document.getElementById("mainMenu");
 
 menuHamburguer.addEventListener("click", function (){
-    
-mainMenu.classList.toggle("menu-hidden")
-})  
+     if (mainMenu.classList.contains("menu-hidden")){
+        mainMenu.classList.add("menu-visible");
+        mainMenu.classList.remove("menu-hidden");
+     } else if (mainMenu.classList.contains("menu-visible")){
+        mainMenu.classList.add("menu-hidden");
+        mainMenu.classList.remove("menu-visible")
+     }
+});  
