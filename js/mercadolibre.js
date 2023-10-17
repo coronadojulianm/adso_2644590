acd1f3db195d6f647ee6464a08b39e16c64a701a
david18012003 <bravocarmen264@gmail.com>
2023-10-17 08:45:47 -0500

    actualizacion 17/10/2023

--- js/mercadolibre.js
+++ js/mercadolibre.js
@@ -10,10 +10,33 @@ let auto1 = new Automovil("Toyota" , "TXL");
 
 alert(auto1.marca + " " + auto1.modelo);
 
-inputBusqueda.addEventListener("keydown", function(){
-    let mainContent = document.getElementById("mainContent");
-    let boxProducto = document.getElementById("div");
+window.addEventListener("load", function(event) {
 
+// inputBusqueda.addEventListener("keydown", function(event){
+
+if(event.key = "Enter"){
+
+    let mainContent = document.getElementById("mainContent");
+    
+    let boxProducto = document.createElement("div");
     mainContent.appendChild(boxProducto);
-    boxProducto.setAttribute("class", "boxProducto");
+    boxProducto.setAttribute("class", "box-producto");
+    
+    let boxImg = document.createElement("div");
+    boxProducto.appendChild(boxImg);
+    boxImg.setAttribute("class", "box-img");
+    let imgAuto = document.createElement("img");
+    boxImg.appendChild(imgAuto);
+    imgAuto.setAttribute("src", "imagenes/toyotaimg.jpg");
+    imgAuto.setAttribute("class", "img-auto");
+    
+    let boxInfo = document.createElement("div");
+    boxProducto.appendChild(boxInfo);
+    boxInfo.setAttribute("class", "box-info");
+    let marcas = document.createElement("label");
+    boxInfo.appendChild(marcas);
+    let txtNodeMarca = document.createTextNode(auto1.marca + " " + auto1.modelo);
+    marcas.appendChild(txtNodeMarca);
+}
+
 });
